56aca285bb3e9e5f17fb0d5e719e356446e4a384
bogdanpasterak <bogdanpasterak@gmail.com>
2021-01-14 00:22:12 +0000

    display binary

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -19,6 +19,11 @@
 # done
  
 # exit 0
+# paraler array background and pair foreground colour
+BG=(40 40 40 100 100 100 47 47 47 107 107 107)
+FG=(90 37 97 30 37 97 97 90 30 37 90 30)
+
+clear
 
 value_character_hex () {
     VAL=$((16#$1)) # patameter hex to dec
@@ -44,7 +49,7 @@ value_character_dec () {
     printf "\e[38;5;%sm\x${CHAR}\e[0m" $COLOR 
 }
 
-
+echo -e "PRINT PALETE from 0 to 255\n"
 for VAL in {0..255}
 do # 24 Colors (gray scale)
     # Display the color 232 240 247 and 255 , 1 of 4 , 6 number
@@ -52,8 +57,12 @@ do # 24 Colors (gray scale)
     COLOR=$((($COLOR * 7) + (($COLOR + 1) / 2) + 232))
     # the number represents one of the 10 ranges
     CHAR=$(($VAL / 24))
-    if [[ $CHAR -eq 10 ]]; then CHAR=9; fi
-    CHAR="3${CHAR}"
+    if [[ $CHAR -eq 10 ]]
+    then
+        CHAR="41"
+    else
+        CHAR="3${CHAR}"
+    fi
     printf "\e[38;5;%sm\x${CHAR}\e[0m" $COLOR 
     # Display 6 colors per lines
     if [ $(($VAL % 64)) == 63 ]
@@ -76,10 +85,31 @@ echo
 # done
  
 echo
-
+COUNT=-1
 hexdump -v -e '/1 "%u\n"' inn | while read DATA
 do
+    ((COUNT=COUNT+1))
+    if [ $COUNT -ge 64 ]
+    then
+        COUNT=0
+        echo
+    fi
     value_character_dec $DATA
 done
+echo
+#                              Foreground Background     
+# printf "\e[%sm\e[%sm Bogdan \e[0m\n" "30" "100"    # black / dark
+# printf "\e[%sm\e[%sm Bogdan \e[0m\n" "90" "47"    # dark / light
+# printf "\e[%sm\e[%sm Bogdan \e[0m\n" "37" "40"    # light / dark
+# printf "\e[%sm\e[%sm Bogdan \e[0m\n" "97" "100"    # white / light
+# echo
+
+
+# black dark light white
+for INDEX in {0..11}
+do
+    printf "\e[%sm\e[%sm Bogdan \e[0m\n" ${FG[$INDEX]} ${BG[$INDEX]} 
+done
+
 
 exit 0
